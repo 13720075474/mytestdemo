65f94aef4febc3733a9851e7143666713b0db9d4
ljw <853453714@qq.com>
2016-03-10 09:48:15 +0800

    3-12

--- demo_iOS 2/AllinpayTest_ObjC/AllinpayTest_ObjC/ViewController.m
+++ demo_iOS 2/AllinpayTest_ObjC/AllinpayTest_ObjC/ViewController.m
@@ -21,6 +21,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"!!!!!!!!!");
+     NSLog(@"!!!!!!!!!"); NSLog(@"!!!!!!!!!"); NSLog(@"!!!!!!!!!"); NSLog(@"!!!!!!!!!"); NSLog(@"!!!!!!!!!"); NSLog(@"!!!!!!!!!");
 }
 
 - (void)viewWillAppear:(BOOL)animated {
